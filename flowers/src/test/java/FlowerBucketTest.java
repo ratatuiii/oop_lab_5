32c49a9cf531a5c299a0e069de68ba99c263f94e
Orest Chupa <orest.chupa@gmail.com>
2024-10-12 21:39:47 +0300

    Implemented FlowerPack and FlowerBucket, modified tests

--- flowers/src/test/java/FlowerBucketTest.java
+++ flowers/src/test/java/FlowerBucketTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.Random;
+import ua.ucu.edu.FlowerPack;
 
 public class FlowerBucketTest {
     private static final Random RANDOM_GENERATOR = new Random();
@@ -25,7 +26,7 @@ public class FlowerBucketTest {
         int price = RANDOM_GENERATOR.nextInt(MAX_PRICE);
         int quantity = RANDOM_GENERATOR.nextInt(MAX_QUANTITY);
         Flower flower = new Rose();
-        flower.setPrice(10);
+        flower.setPrice(price);
         FlowerPack flowerPack = new FlowerPack(flower, quantity);
         flowerBucket.add(flowerPack);
         Assertions.assertEquals(price * quantity, flowerBucket.getPrice());
